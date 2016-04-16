be3165ad7d2632786e5e7ce3cc548f93d16d048f
Ernest McCracken <emccrckn@gmail.com>
2016-04-16 16:00:47 -0500

    New robot kyle with hitboxes

---
+++ GunJam/Assets/Game Jam/HitTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitTest : MonoBehaviour {
+
+    public float dmgModifier;
+    public int damage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Actor actor = GetComponentInParent<Actor>();
+        actor.TakeDamage(damage * dmgModifier);
+        Destroy(collision.collider.gameObject);
+    }
+}
