4296b95f2ff8de037aa2de8e0fdf3d2be2b371c4
Jonathan Schrack <jmschrack>
2016-04-20 13:10:36 -0500

    Finished lazer gun. Hilarious.

--- GunJam/Assets/Scripts/Lazer.cs
+++ GunJam/Assets/Scripts/Lazer.cs
@@ -3,29 +3,56 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Lazer : Weapon {
+    bool isFiring=false;
+    bool isOverheated=false;
     public float heatLevel;
     public float normalMax;
     public float optimumMax;
+    public float coolDownRate=4;
+    public float heatUpRate=5;
+    public GameObject beam;
     
     public Image heatMeter;
 
-	// Use this for initialization
-	void Start () {
 	
-	}
+    //this feels like a hack, but it stays true to the same logic everything else uses
+    //pull trigger to shoot. In this case, we have a listener to stop shooting on trigger release.
 	public override void shoot(){
-        
+        isFiring=!isOverheated;
+        beam.SetActive(isFiring);
     }
 	// Update is called once per frame
 	void Update () {
-	    heatLevel-=Time.deltaTime*4.0f;
-        if(heatLevel<0)
-            heatLevel=0;
+        handleInput();
+        isOverheated=isOverheated||heatLevel>100;
+        if(isOverheated){
+            isFiring=false;
+            beam.SetActive(isFiring);
+        }
+            
+        if(isFiring){
+            heatLevel+=Time.deltaTime*heatUpRate;
+        }else{
+            heatLevel-=Time.deltaTime*coolDownRate;
+            if(heatLevel<0){
+                heatLevel=0;
+                isOverheated=false;
+            }
+                    
+        }
+	    handleUI();
             
 	}
     
+    void handleInput(){
+        if(PlayerControls.getPlayer().GetButtonUp("FireGun")){
+            isFiring=false;
+            beam.SetActive(isFiring);
+        }
+        
+    }
     void handleUI(){
-        heatMeter.fillAmount=heatLevel;
+        heatMeter.fillAmount=heatLevel/100;
         if(heatLevel<100)
             heatMeter.color=Color.red;
         if(heatLevel<90)
